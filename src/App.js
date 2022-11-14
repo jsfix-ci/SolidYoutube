af6a90832935694d371e90fdc6eb836f4ed78d35
meljum <110549008+meljum@users.noreply.github.com>
2022-11-14 18:24:18 +0600

    fix: conflict

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-=======
-import './App.css';
 import { useTranslation } from 'react-i18next';
->>>>>>> 615a432 (feat: add i18next)
 
 function App() {
   const { t} = useTranslation();
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-=======
       {t('greeting')}
->>>>>>> 615a432 (feat: add i18next)
     </div>
   );
 }
