14eb030ac64927c0d7b619aaf25bcf5b1ed54051
sabit2717 <medetov.sabit@mail.ru>
2022-11-14 20:11:48 +0600

    feat/setup/eslint-prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import './App.css';
-import { useTranslation } from 'react-i18next';
+import './App.css'
+import { useTranslation } from 'react-i18next'
 
 function App() {
-  const { t} = useTranslation();
-
-  return (
-    <div className="App">
-      {t('greeting')}
-    </div>
-  );
+  const { t } = useTranslation()
+  return <div className='App'>{t('greeting')}</div>
 }
-
-export default App;
+export default App
