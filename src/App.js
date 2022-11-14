45e18090000e03cccc52c7c98fed2091eaac5745
meljum <110549008+meljum@users.noreply.github.com>
2022-11-14 17:11:58 +0600

    fix:comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
-import {
-  Routes,
-  Route,
-  } from 'react-router-dom';
-import HomePage from "./pages/homePage/HomePage";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/HomePage.jsx' element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </div>
   );
