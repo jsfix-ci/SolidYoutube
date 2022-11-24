9501225c865203169f0bc707b71613f27703674d
meljum <110549008+meljum@users.noreply.github.com>
2022-11-24 22:21:32 +0600

    added get api youtube

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
-import { API, base_url } from '../constant'
+import { API, base_url, ChannelId } from '../constant'
 
 const http = axios.create({
   baseURL: base_url,
 })
 
 const Api = {
-  getVideo: () => http.get(`video?key=${API}`),
+  getVideo: () =>
+    http.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${API}&channelid=${ChannelId}&part=snippet,id&order=date&maxResults=5`
+    ),
 }
 export default Api
--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const base_url = 'https://developers.google.com/youtube/v3/'
 export const API = 'AIzaSyBeHciUdw3CLu-bptCUcvXzCg_uJxYDAls'
-// const ChannelId = "UCJyEBMU1xVP2be1-AoGS1BA "
+export const ChannelId = 'UCJyEBMU1xVP2be1-AoGS1BA '
 console.log(base_url)
