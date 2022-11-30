fbba202e4a4033ea43a54049c79c2c6c98db027b
Nazira12 <nsvarova12@gmail.com>
2022-11-30 15:44:33 +0600

    feat: firebase integration

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
-import { API, base_url, ChannelId } from '../constant'
+// import axios from 'axios'
+// import { API, base_url, ChannelId } from '../constant'
 
-const http = axios.create({
-  baseURL: base_url,
-})
+// const http = axios.create({
+//   baseURL: base_url,
+// })
 
-const Api = {
-  getVideo: () =>
-    http.get(
-      `https://www.googleapis.com/youtube/v3/search?key=${API}&channelid=${ChannelId}&part=snippet,id&order=date&maxResults=5`
-    ),
-}
-export default Api
+// const Api = {
+//   getVideo: () =>
+//     http.get(
+//       `https://www.googleapis.com/youtube/v3/search?key=${API}&channelid=${ChannelId}&part=snippet,id&order=date&maxResults=5`
+//     ),
+// }
+// export default Api
--- src/index.js
+++ src/index.js
@@ -7,6 +7,34 @@ import { store } from './redux'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import './i18n'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDm9xO6N3VHS3I0acoPFd6OSSVyuP4gq2c',
+  authDomain: 'solid-team-ab334.firebaseapp.com',
+  projectId: 'solid-team-ab334',
+  storageBucket: 'solid-team-ab334.appspot.com',
+  messagingSenderId: '843780543210',
+  appId: '1:843780543210:web:4e0542f88518f63949bd93',
+  measurementId: 'G-M5V7Y4DJWG',
+}
+
+initializeApp(firebaseConfig)
+const db = getFirestore()
+const colRef = collection(db, 'books')
+
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
